4791ff7b5e6dcfa16fe10c06d72ee7eccf06e26c
Shazidkhan131313khan <shazidkhan1870187@gmail.com>
2026-01-31 01:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Backend is running 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
