b9c16abe159f10444b9ea2f010935ed74108ead1
Shazidkhan131313khan <shazidkhan1870187@gmail.com>
2026-01-31 00:58:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/process", methods=["POST"])
+def process():
+    data = request.json
+    return jsonify({
+        "status": "success",
+        "message": "Backend connected successfully",
+        "url": data.get("url")
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
